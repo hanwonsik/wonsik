21d95475673beee572c16d7025b2d7bbe8e561f8
wonsik <oaoappp@hanmail.net>
2016-12-15 16:07:32 +0900

    sound

---
+++ 01_SoundControl/Assets/Script/SoundObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundObject : MonoBehaviour {
+
+    [SerializeField]
+    SOUNDTYPE    m_SoundType;
+
+	void Awake()
+    {
+        SoundMgr.Instance.AddObject(m_SoundType, gameObject);
+	}
+    void OnDestroy()
+    {
+        SoundMgr.Instance.RemoveObject(m_SoundType, gameObject);
+    }
+}
