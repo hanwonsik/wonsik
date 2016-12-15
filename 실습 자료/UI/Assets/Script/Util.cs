f37f60aaca63162cf526aff449aa8fc174f2bf5a
wonsik <oaoappp@hanmail.net>
2016-12-15 16:10:26 +0900

    d

---
+++ 실습 자료/UI/Assets/Script/Util.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Util : MonoBehaviour
+{
+    public void LoadScene(string _Name)
+    {
+        SceneManager.LoadScene(_Name);
+    }
+    public void UnLoadScene(string _Name)
+    {
+        SceneManager.UnloadScene(_Name);
+    }
+    public void PopupScene(string _Name)
+    {
+        SceneManager.LoadScene(_Name, LoadSceneMode.Additive);
+    }
+}
