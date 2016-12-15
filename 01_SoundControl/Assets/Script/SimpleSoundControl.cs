8ee9e2877244e720e086fdb985fcee4e3dcb935d
wonsik <oaoappp@hanmail.net>
2016-12-15 15:29:33 +0900

    unity sound

---
+++ 01_SoundControl/Assets/Script/SimpleSoundControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleSoundControl : MonoBehaviour {
+
+    [SerializeField]
+    string[] m_TagName = new string[(int)SOUNDTYPE.SOUND_MAXCOUNT];
+
+    public void SetVolume(SOUNDTYPE _Type, float _Vol)
+    {
+        GameObject[] list = GameObject.FindGameObjectsWithTag(m_TagName[(int)_Type]);
+
+        foreach( GameObject Obj in list )
+        {
+            AudioSource Audio = Obj.GetComponent<AudioSource>();
+
+            if( Audio != null )
+            {
+                Audio.volume = _Vol;
+            }
+        }
+    }
+}
