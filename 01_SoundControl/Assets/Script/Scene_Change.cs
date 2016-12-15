fe61c1008c3c5807b8608a8d9780a0ab1c367503
wonsik <oaoappp@hanmail.net>
2016-12-15 15:25:41 +0900

    unity sound

---
+++ 01_SoundControl/Assets/Script/Scene_Change.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene_Change : MonoBehaviour {
+
+    public void LoadScene(string _Name)
+    {
+        SceneManager.LoadScene(_Name);
+    }
+    public void UnLoadScene(string _Name)
+    {
+        SceneManager.UnloadScene(_Name);
+    }
+    public void PopUpScene(string _Name)
+    {
+        SceneManager.LoadScene(_Name, LoadSceneMode.Additive);
+    }
+}
