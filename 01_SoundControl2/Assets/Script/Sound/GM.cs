ce10cd4f585e3cc1d9711becaa92205b94b7335f
wonsik <oaoappp@hanmail.net>
2016-12-22 16:06:24 +0900

    dsa

---
+++ 01_SoundControl2/Assets/Script/Sound/GM.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections;
+
+public class GM : MonoBehaviour
+{
+   /*int         count;
+      int         Leght;
+      bool        Mute;
+
+      void Start()
+      {}
+      public void ChangeSound(bool Mute)
+      {
+          if (Mute == false)
+          {
+              Debug.Log("Vol Off");
+              AudioListener.pause = true;
+              AudioListener.volume = 1;
+              Mute = true;
+          }
+          else
+          {
+              Debug.Log("Vol On");
+              AudioListener.pause = false;
+              AudioListener.volume = 0;
+              Mute = false;
+          }
+
+
+    public AudioSource[] audioSourcesToIgnore;
+    private bool pause;
+
+    void Start()
+    { 
+        foreach (AudioSource snd in audioSourcesToIgnore)
+        {  snd.ignoreListenerPause = true;}
+    }
+
+void OnGUI()
+    { //OnGUI because I also had some clickable buttons in my pause menu
+
+       if (pause)
+        {
+        Time.timeScale = 0.0f;
+        AudioListener.pause = true;
+     }
+       else{
+            Time.timeScale = 1.0f;
+            AudioListener.pause = false;
+        }
+    }*/
+}
