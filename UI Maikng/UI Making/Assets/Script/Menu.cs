b928042e344eca17ad54d048c26249bf44e2f1e6
hanwonsik <oaoappp@hanmail.net>
2016-12-26 00:47:07 +0900

    환경설정(슬라이드)

---
+++ UI Maikng/UI Making/Assets/Script/Menu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject mainMenuHolder;
+    public GameObject OptionMenuHolder;
+
+    public Slider[] volumeSlider;
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+	
+    public void Option()
+    {
+        mainMenuHolder.SetActive(false);
+        OptionMenuHolder.SetActive(true);
+    }
+    public void MainMenu()
+    {
+        mainMenuHolder.SetActive(true);
+        OptionMenuHolder.SetActive(false);
+
+    }
+    public void SetMasterVolume(float value)
+    {
+      
+    }
+    public void SetEffectVolume(float value)
+    {
+
+    }
+}
