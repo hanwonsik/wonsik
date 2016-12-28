43774dbe63cf08ad448e9314fe14f361c8cfa680
wonsik <oaoappp@hanmail.net>
2016-12-28 16:49:51 +0900

    fds

---
+++ UI Maikng3/UI Making/Assets/Script/Singlton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenericSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static object m_SyncRoot = new object();
+    private static T m_Instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {
+                lock (m_SyncRoot)
+                {
+                    if (m_Instance == null)
+                    {
+                        //  해당 스크립트를 찾는다.
+                        m_Instance = FindObjectOfType<T>();
+
+                        if (m_Instance == null)
+                        {
+                            GameObject Obj = new GameObject();
+                            Obj.name = "Singleton : " + typeof(T).ToString();
+
+                            m_Instance = Obj.AddComponent<T>();
+                        }
+
+                        //  Init 메서드 호출
+                        m_Instance.SendMessage("Init", SendMessageOptions.DontRequireReceiver);
+
+                        //  씬전환시 객체 유지
+                        DontDestroyOnLoad(m_Instance);
+                    }
+                }
+            }
+            return m_Instance;
+        }
+    }
+}
